097147ee690c1129ee2de67398c1fcc6b6b4e2b0
ibtsam <aslamibtsam@gmail.com>
2020-02-13 00:40:37 +0500

    Update MainActivity.java

--- app/src/main/java/islo/hawk/moviesappstage1/MainActivity.java
+++ app/src/main/java/islo/hawk/moviesappstage1/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity implements MoviesAdapter.Mov
     private RecyclerView recyclerView;
     private  GridLayoutManager gridLayoutManager;
 
-
+//comment from the github
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
